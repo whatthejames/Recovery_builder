82a98888da193e91920845eddcf659c421892a2c
whatthejames <124553067+whatthejames@users.noreply.github.com>
2023-06-13 20:38:23 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@ export DEVICE="m10lte" # codename used in device tree
 export DT_LINK="https://github.com/twrpdtgen/android_device_infinix_Infinix-X676C" # device tree link
 export DT_BRANCH="vext_x676c_h891-user-12-SP1A.210812.016-203357-release-keys" # device tree branch
 export VENDOR="infinix" # device manufacturer or vendor
-export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
+export TARGET="bootimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
