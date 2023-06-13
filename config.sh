ce3aecd6db5807f7cd5c8971d0c1a706f00d08e9
whatthejames <124553067+whatthejames@users.noreply.github.com>
2023-06-13 20:43:54 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,11 +2,11 @@
 
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git" # the link of manifest
+export BRANCH="twrp-12.1" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
+export DEVICE="Infinix-X676C" # codename used in device tree
 export DT_LINK="https://github.com/twrpdtgen/android_device_infinix_Infinix-X676C" # device tree link
 export DT_BRANCH="vext_x676c_h891-user-12-SP1A.210812.016-203357-release-keys" # device tree branch
 export VENDOR="infinix" # device manufacturer or vendor
