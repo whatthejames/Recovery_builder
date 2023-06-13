775484365d54f374e8895180ba7597bc5dea493b
whatthejames <124553067+whatthejames@users.noreply.github.com>
2023-06-13 20:36:46 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,9 +7,9 @@ export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
 export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export DT_LINK="https://github.com/twrpdtgen/android_device_infinix_Infinix-X676C" # device tree link
+export DT_BRANCH="vext_x676c_h891-user-12-SP1A.210812.016-203357-release-keys" # device tree branch
+export VENDOR="infinix" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
